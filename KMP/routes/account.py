209164c4aa29b7ea584acb51c2899c89ecef4039
simowardi <daoist32@gmail.com>
2024-10-16 04:54:04 +0100

    small route update

--- KMP/routes/account.py
+++ KMP/routes/account.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from models import db, Giveaway, Participation, User, Winner
+from models import db, User
 from flask_login import login_required, current_user, logout_user
 from datetime import datetime
 
