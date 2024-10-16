88874581d2145977ca4bd4dbea70d70850ba9441
simowardi <daoist32@gmail.com>
2024-10-16 05:42:06 +0100

    fix minor bugs

--- KMP/routes/__init__.py
+++ KMP/routes/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 # Initialize the blueprints
 auth_bp = Blueprint('auth', __name__)
 account_bp = Blueprint('account', __name__)
-
+keyword_bp = Blueprint('keyword', __name__)
 
 # Import routes to register them with blueprints
 from .auth import auth_bp
