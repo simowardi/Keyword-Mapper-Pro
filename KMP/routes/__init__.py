fd71d2d587cb059b76eaba5294e992d104dcaf18
simowardi <daoist32@gmail.com>
2024-10-16 05:46:29 +0100

    fix minor bugs

--- KMP/routes/__init__.py
+++ KMP/routes/__init__.py
@@ -7,6 +7,7 @@ auth_bp = Blueprint('auth', __name__)
 account_bp = Blueprint('account', __name__)
 keyword_bp = Blueprint('keyword', __name__)
 
+
 # Import routes to register them with blueprints
 from .auth import auth_bp
 from .account import account_bp
