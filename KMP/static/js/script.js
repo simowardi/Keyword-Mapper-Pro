30ed9a6a3756c7c884a3c7e5c3c4b9fcf1052ba5
simowardi <daoist32@gmail.com>
2024-10-20 19:53:22 +0100

    update html exlorer

--- KMP/static/js/script.js
+++ KMP/static/js/script.js
@@ -1,3 +1,41 @@
+/**
+ * Copies the text from the textarea with the given ID to the clipboard.
+ * @param {string} textareaId - The ID of the textarea from which to copy the text.
+ */
+function copyToClipboard(textareaId) {
+    const textarea = document.getElementById(textareaId);
+    textarea.select();
+    document.execCommand('copy');
+
+    // Flash effect
+    const button = event.target;
+    button.style.backgroundColor = 'green';
+
+    // Reset the background color after half a second
+    setTimeout(() => {
+        button.style.backgroundColor = ''; // Reset to original
+    }, 500); // Change to 500 milliseconds (half a second)
+}
+
+
+document.querySelector('.logout').addEventListener('click', function() {
+	if (confirm('Are you sure you want to logout?')) {
+	  // Perform logout action here
+	  alert('Logged out successfully');
+	}
+});
+
+
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  this.innerHTML = document.body.classList.contains('dark-mode') ? '☀️ Day Mode' : '🌙 Night Mode';
+});
+
+
+
+// Handle the keyword filtering process
+
 document.addEventListener('DOMContentLoaded', function() {
     const kwListTextarea = document.getElementById('keys_to_be_matched');
     const positiveKwTextarea = document.getElementById('keys_to_match');
@@ -42,42 +80,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-/**
- * Copies the text from the textarea with the given ID to the clipboard.
- * @param {string} textareaId - The ID of the textarea from which to copy the text.
- */
-function copyToClipboard(textareaId) {
-    const textarea = document.getElementById(textareaId);
-    textarea.select();
-    document.execCommand('copy');
-
-    // Flash effect
-    const button = event.target;
-    button.style.backgroundColor = 'green';
-
-    // Reset the background color after half a second
-    setTimeout(() => {
-        button.style.backgroundColor = ''; // Reset to original
-    }, 500); // Change to 500 milliseconds (half a second)
-}
-
-
-document.querySelector('.logout').addEventListener('click', function() {
-	if (confirm('Are you sure you want to logout?')) {
-	  // Perform logout action here
-	  alert('Logged out successfully');
-	}
-  });
-
-
-const themeToggle = document.querySelector('.theme-toggle');
-themeToggle.addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
-  this.innerHTML = document.body.classList.contains('dark-mode') ? '☀️ Day Mode' : '🌙 Night Mode';
-});
 
 
 
+// Handle the keyword exploration process
 
 document.getElementById('suggestionsForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
@@ -103,3 +109,18 @@ document.getElementById('mainkeywords').addEventListener('input', function() {
 	var keywordCount = this.value.split('\n').filter(line => line.trim() !== '').length;
 	document.getElementById('keywordCount').textContent = keywordCount + ' keywords';
 });
+
+
+
+
+// Handle the keyword grouping process
+
+
+
+
+
+// handle the keyword intent analyzer
+
+
+
+
