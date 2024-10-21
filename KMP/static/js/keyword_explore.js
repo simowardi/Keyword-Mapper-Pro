5e88400b72666201a3503fd139a4f3dba1039e14
simowardi <daoist32@gmail.com>
2024-10-21 13:39:49 +0100

    update html explorer js & css

---
+++ KMP/static/js/keyword_explore.js
@@ -0,0 +1,71 @@
+/**
+ * Copies the text from the textarea with the given ID to the clipboard.
+ * @param {string} textareaId - The ID of the textarea from which to copy the text.
+ */
+function copyToClipboard(textareaId) {
+    const textarea = document.getElementById(textareaId);
+    textarea.select();
+    document.execCommand('copy');
+
+    // Flash effect for the button
+    const button = event.target;
+    button.style.backgroundColor = 'green';
+
+    // Reset the background color after half a second
+    setTimeout(() => {
+        button.style.backgroundColor = ''; // Reset to original
+    }, 500); // Change to 500 milliseconds (half a second)
+}
+
+// Logout confirmation
+document.querySelector('.logout').addEventListener('click', function() {
+    if (confirm('Are you sure you want to logout?')) {
+        // Perform logout action here
+        alert('Logged out successfully');
+    }
+});
+
+// Theme toggle between dark mode and light mode
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.innerHTML = document.body.classList.contains('dark-mode') ? '☀️ Day Mode' : '🌙 Night Mode';
+});
+
+
+// Count keywords based on input
+document.getElementById('mainkeywords').addEventListener('input', function() {
+    var keywordCount = this.value.split('\n').filter(line => line.trim() !== '').length;
+    document.getElementById('keywordCount').textContent = keywordCount + ' keywords';
+});
+
+
+
+// Handle the keyword exploration process
+$(document).ready(function() {
+    $('#suggestionsForm').on('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+        
+        var formData = $(this).serialize(); // Serialize form data
+        
+        $.ajax({
+            url: "{{ url_for('keyword.keyword_explorer') }}",
+            type: "POST",
+            data: formData,
+            success: function(response) {
+                if (response.suggestions && response.suggestions.length > 0) {
+                    var keywords = response.suggestions.join('\n');
+                    $('#resultKeywords').val(keywords);
+                    $('#suggestedKeywordCount').text(response.suggestions.length + ' keywords suggested');
+                    $('.results-section').show();
+                } else {
+                    $('#resultKeywords').val('No keywords found.');
+                    $('#suggestedKeywordCount').text('0 keywords suggested');
+                }
+            },
+            error: function(error) {
+                console.log("Error:", error);
+            }
+        });
+    });
+});
