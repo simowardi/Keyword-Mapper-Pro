b9bd49699e3cb1037ec6f85a61746becefb9b0aa
simowardi <daoist32@gmail.com>
2024-10-22 13:10:11 +0100

    app.py add some doc

--- KMP/app.py
+++ KMP/app.py
@@ -14,19 +14,20 @@ login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
 
+# Configure app with Flask-Login settings (see routes/__init__.py)
 app.config['SECRET_KEY'] = os.environ.get('az12', '123456789AZERTYUIOP')
 
 
-# Configure SQLAlchemy
+# Configure SQLAlchemy with app config settings (see models/__init__.py)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user11:ZE2323@localhost/my_flask_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# Initialize app with SQLAlchemy
+# Initialize app with SQLAlchemy (see models/__init__.py)
 init_app(app)
 
 
-# Register blueprints
+# Register blueprints (see routes/__init__.py)
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(account_bp, url_prefix='/account')
 app.register_blueprint(keyword_bp, url_prefix='/keyword')
@@ -34,6 +35,14 @@ app.register_blueprint(keyword_bp, url_prefix='/keyword')
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    User loader function for Flask-Login.
+    Loads a user by its ID for login session management.
+    Parameters:
+        user_id (int): The ID of the user to load.
+    Returns:
+        The loaded User object, or None if not found.
+    """
     return db.session.get(User, int(user_id))
 
 
@@ -75,8 +84,17 @@ def dashboard():
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
+    """Serves static files from the 'static' folder in the application root.
+    Given a filename, this function will look for the file in the 'static' folder
+    and return it if it exists. Otherwise, a 404 error is returned.
+    Args:
+        filename (str): The name of the file to return.
+    Returns:
+        The requested file if it exists, otherwise a 404 error.
+    """
     root_dir = os.path.dirname(os.path.abspath(__file__))
     return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
 
 
 # Only initialize database tables if running as the main application
